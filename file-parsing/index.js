7ed3fb0e2e7d7a7c8514a4a93450b010471be90b
Chaofan Chen <chenchaofan@ubuntu.(none)>
2014-01-22 20:58:37 -0600

    implement a simple file parser - Note: only 34 items are added at the moment (1 assertion failed)

--- file-parsing/index.js
+++ file-parsing/index.js
@@ -7,6 +7,25 @@ var path = require('path');
 // text file was generated with pdftotext -layout. You can regenerate it easily. It's just
 // there for convinience.
 
+var parseLine = function (line, results) {
+    line = line.replace(/^\s+|\s+$/g, '');
+    line = line.replace(/\s{2,}/g, ' ');
+
+    var words = line.split(' ');
+
+    var index;
+    if ((/^\d+$/.test(words[0])) && (/^\d+$/.test(words[1]))) {
+	results.items.push({});
+	index = results.items.length - 1;
+	results.items[index]["quantity"] = words[0];
+	results.items[index]["unitPrice"] = words[words.length-2];
+	results.items[index]["extendedPrice"] = words[words.length-1];
+    }
+    else if (words[0] === "Mfg#:") {
+	index = results.items.length - 1;
+	results.items[index]["mfgPart"] = words[1];
+    }
+};
 
 var parseText = function (text, callback) {
     var results = {
@@ -20,11 +39,18 @@ var parseText = function (text, callback) {
     };
     // Start by looking at the converted text file manually. See what patterns you can find
     // and start with simple a RegExp to pull those out.
-    callback(new Error('Parse Not Implemented!'));
+    // callback(new Error('Parse Not Implemented!'));
+
+    var lines = text.split('\n');
+    var i;
+
+    for (i = 0; i < lines.length; i++) {
+	parseLine(lines[i], results);
+    }
 
     // Once you have results, you can return them by passing them back to the callback, with null
     // as the error value:
-    // callback(null, results);
+    callback(null, results);
 };
 
 var parse = function (filename, callback) {
